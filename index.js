3c7f17de06a2cc4439a89f5b6b4eb99c72527575
Fatima Nava <94cabanel@gmail.com>
2022-03-16 22:28:31 -0700

    removed annualExpense

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-window.prompt(weeklyExpenseQuestions[0])
+for(let i = 0; i< weeklyExpenseQuestions.length; i++ ){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i=0; i< monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
